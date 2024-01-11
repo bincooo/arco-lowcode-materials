0d18fad79c73df58e78d528dd3b32296388aafe3
折戟沉沙、丿 <36452456+bincooo@users.noreply.github.com>
2024-01-12 00:48:54 +0800

    commit pages

--- src/components/popover/index.tsx
+++
@@ -1,9 +0,0 @@
-import { forwardRef, Ref } from 'react';
-import { Popover } from '@arco-design/web-react';
-import { withSingleChild } from '../../utils'
-
-const PopoverBox = (props: any, ref: Ref<any>) => {
-    return withSingleChild(Popover, props, ref)
-}
-
-export default forwardRef(PopoverBox)
